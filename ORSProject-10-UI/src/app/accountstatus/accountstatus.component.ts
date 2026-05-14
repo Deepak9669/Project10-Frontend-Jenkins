2b2d1dec34f1f5cf33dcecd22e1a5c7830911562
Deepak verma <deepakverma726800@gmail.com>
2026-05-14 11:13:18 +0530

    done

--- ORSProject-10-UI/src/app/accountstatus/accountstatus.component.ts
+++ ORSProject-10-UI/src/app/accountstatus/accountstatus.component.ts
@@ -3,6 +3,7 @@ import { BaseCtl } from '../base.component';
 import { ServiceLocatorService } from '../service-locator.service';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-accountstatus',
   templateUrl: './accountstatus.component.html'
